176ecc608d1a721d5913f94625a7cb29219f24aa
Shankar <sikhakollishankar16@gmail.com>
2024-05-12 11:41:00 +0530

    find the medain

--- find the median.py
+++ find the median.py
@@ -7,3 +7,8 @@ class Solution:
         else:
             median = (v[n//2])
         return median
+    
+'''
+    median of the array is the average of middle two terms if number of elements is even
+    is the middle element is the number of elements if number of elements is odd
+'''
