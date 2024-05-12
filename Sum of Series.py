51e72c8943f074bd37fe1fd8a9d2290529bc59d4
Shankar <sikhakollishankar16@gmail.com>
2024-05-12 11:30:21 +0530

    Sum of Series

---
+++ Sum of Series.py
@@ -0,0 +1,9 @@
+class Solution:
+    def seriesSum(self, n : int) -> int:
+        return int(n*(n+1)//2)
+        
+
+'''
+    The formula for calculating the sum of first n natural numbers is n*(n+1)/2
+    
+'''
